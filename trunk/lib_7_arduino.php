d6ff56e2a42fb0574e35a2417b9b586cfe4755d5
benny.saxen <benny.saxen@e1aef1b2-8df6-a3cb-9e93-f329396b0b09>
2012-08-19 10:08:10 +0000

    email check

--- trunk/lib_7_arduino.php
+++ trunk/lib_7_arduino.php
@@ -273,41 +273,18 @@ function check_email_address($email)
 //==========================================
 {
 
-  // First, we check that there's one @ symbol, 
-  // and that the lengths are right.
-  if (!ereg("^[^@]{1,64}@[^@]{1,255}$", $email)) {
-    // Email invalid because wrong number of characters 
-    // in one section or wrong number of @ symbols.
-    return false;
-  }
-  // Split it into sections to make life easier
-  $email_array = explode("@", $email);
-  $local_array = explode(".", $email_array[0]);
-  for ($i = 0; $i < sizeof($local_array); $i++) {
-    if
-      (!ereg("^(([A-Za-z0-9!#$%&'*+/=?^_`{|}~-][A-Za-z0-9!#$%&
-↪'*+/=?^_`{|}~\.-]{0,63})|(\"[^(\\|\")]{0,62}\"))$",
-	     $local_array[$i])) {
-      return false;
-    }
-  }
-  // Check if domain is IP. If not, 
-  // it should be valid domain name
-  if (!ereg("^\[?[0-9\.]+\]?$", $email_array[1])) {
-    $domain_array = explode(".", $email_array[1]);
-    if (sizeof($domain_array) < 2) {
-      return false; // Not enough parts to domain
-    }
-    for ($i = 0; $i < sizeof($domain_array); $i++) {
-      if
-	(!ereg("^(([A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9])|
-↪([A-Za-z0-9]+))$",
-	       $domain_array[$i])) {
-        return false;
-      }
-    }
-  }
-  return true;
+  $regex = $strict? 
+      '/^([.0-9a-z_-]+)@(([0-9a-z-]+\.)+[0-9a-z]{2,4})$/i' : 
+       '/^([*+!.&#$�\'\\%\/0-9a-z^_`{}=?~:-]+)@(([0-9a-z-]+\.)+[0-9a-z]{2,4})$/i' ; 
+  if (preg_match($regex, trim($email), $matches)) 
+    { 
+      return true; 
+    } 
+  else 
+    { 
+      return false; 
+    } 
+  
 }
 
 //==========================================
